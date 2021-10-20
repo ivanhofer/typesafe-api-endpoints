de0ce252826f7dd288d317d72f9b6f0a6e78f846
Ivan Hofer <ivan.hofer@outlook.com>
2021-10-20 10:56:53 +0200

    fix: export Errors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { createTypesafeApiEndpointsClient } from './connectors/connector.client'
 export { createTypesafeApiEndpointsServer } from './connectors/connector.server'
+export { ApiError, EndpointNotFoundError } from './errors'
 export type { CreateTypesafeApiEndpointsSchema, MethodsFromSchema, EndpointStringsFromSchema } from './types/types'
