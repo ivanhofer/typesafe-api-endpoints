685c52c3426535901cfc89bdb83a44b736c2b493
Ivan Hofer <ivan.hofer@outlook.com>
2021-10-21 08:30:33 +0200

    chore: update base example

--- example/schema.ts
+++ example/schema.ts
@@ -3,18 +3,48 @@ import { createTypesafeApiEndpointsServer } from '../src/connectors/connector.se
 import type { CreateTypesafeApiEndpointsSchema } from '../src/types/types'
 import { ServerHandler } from '../src/types/types.server'
 
+// dummy data  --------------------------------------------------------------------------------------------------------
+
+const product1: Product = {
+	id: 0,
+	name: 'p1',
+	color: 'red'
+}
+
+const product2: Product = {
+	id: 1,
+	name: 'p1',
+	color: 'green'
+}
+
+const product3: Product = {
+	id: 2,
+	name: 'p3',
+	color: 'blue'
+}
+
+let products = [product1, product2, product3]
+
 // schema -------------------------------------------------------------------------------------------------------------
+
+type Product = {
+	id: number
+	name: string
+	color: 'red' | 'green' | 'blue'
+}
 
 type Schema = CreateTypesafeApiEndpointsSchema<{
 	GET: {
-		test: number
-		test123: number
-		'test/{id}': string
-		'test/{id}/subpath?filter&search': { test: string }
-		'test/{id}/subpath/{subId}?test&asd': { id: string; subId: string }
+		'products': Product[]
+		'products?limit': Product[]
+		'product/{id}': Product
+		'products?search': Product[]
 	}
 	POST: {
-		'product/{productId}': (t: { color: string }) => boolean
+		'product': (product: Product) => boolean
+	},
+	PUT: {
+		'product/{productId}': (product: Partial<Product>) => boolean
 	}
 }>
 
@@ -22,25 +52,53 @@ type Schema = CreateTypesafeApiEndpointsSchema<{
 
 const handler: ServerHandler<Schema> = {
 	GET: {
-		"test/{id}": async () => null as any,
-		"test": async () => null as any,
-		"test/{id}/subpath/{subId}?test&asd": async () => null as any,
-		"test/{id}/subpath?filter&search": async () => null as any,
-		"test123": async () => null as any,
+		"products": async () => products,
+		"products?limit": async ({ query: { limit = Infinity } }) => products.slice(0, +limit),
+		"product/{id}": async ({ slugs: { id } }) => products.find(({ id: productId }) => +id === productId),
+		"products?search": async () => { throw new Error('not yet implemented') }
 	},
 	POST: {
-		"product/{productId}": async () => null as any
+		"product": async ({ body: product }) => {
+			products = [...products, product]
+			return true
+		}
+	},
+	PUT: {
+		"product/{productId}": async ({ slugs: { productId }, body: product }) => {
+			let updated = false
+			products = products.map(original => {
+				let newValue: Product
+				if (original.id === +productId) {
+					newValue = { ...original, ...product }
+					updated = true
+				} else {
+					newValue = original
+				}
+				return newValue
+			})
+			return updated
+		}
 	}
 }
 
 const typesafeApiEndpointsServer = createTypesafeApiEndpointsServer<Schema>(handler)
 
-const a = typesafeApiEndpointsServer.GET('test', new URLSearchParams(), null as never)
-const a1 = typesafeApiEndpointsServer.POST('product/{productId}', new URLSearchParams(), { color: '' })
+// you will probably write a generic wrapper around this
+
+const s1 = typesafeApiEndpointsServer.GET('products', new URLSearchParams(), null as never)
+const s2 = typesafeApiEndpointsServer.GET('product/{id}', new URLSearchParams('id=1'), null as never)
+const s3 = typesafeApiEndpointsServer.PUT('product/{productId}', new URLSearchParams('productId=1'), { color: 'green' })
 
 // client -------------------------------------------------------------------------------------------------------------
 
 const typesafeApiEndpointsClient = createTypesafeApiEndpointsClient<Schema>('api/', fetch)
 
-const x1 = typesafeApiEndpointsClient.GET('test', undefined)
-const x2 = typesafeApiEndpointsClient.POST('product/{productId}', { slugs: { productId: '12' }, query: undefined }, { color: '12' })
+const fetchData = async () => {
+	const { data: products } = await typesafeApiEndpointsClient.GET('products', undefined)
+
+	const { data: product1 } = await typesafeApiEndpointsClient.GET('product/{id}', { slugs: { id: '2' }, query: undefined })
+
+	const { error } = await typesafeApiEndpointsClient.GET('products?search', { query: { search: 'Test product' } })
+
+	const { data: updateSuccessful } = await typesafeApiEndpointsClient.PUT('product/{productId}', { slugs: { productId: '1' }, query: undefined }, { color: 'green' })
+}
