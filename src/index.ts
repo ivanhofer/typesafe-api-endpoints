4a3ec39f4d28d191da472e1409e55ec2351b8cb2
Ivan Hofer <ivan.hofer@outlook.com>
2021-10-22 08:25:44 +0200

    expose type `ServerHandler`

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { createTypesafeApiEndpointsServer } from './connectors/connector.server'
 import { ApiError, EndpointNotFoundError } from './errors'
 import type { CreateTypesafeApiEndpointsSchema, MethodsFromSchema, EndpointStringsFromSchema } from './types/types'
 import type { ApiResponse } from './types/types.client'
+import type { ServerHandler } from './types/types.server'
 
 export { createTypesafeApiEndpointsClient, createTypesafeApiEndpointsServer, ApiError, EndpointNotFoundError }
-export type { CreateTypesafeApiEndpointsSchema, MethodsFromSchema, EndpointStringsFromSchema, ApiResponse }
+export type { CreateTypesafeApiEndpointsSchema, MethodsFromSchema, EndpointStringsFromSchema, ApiResponse, ServerHandler }
